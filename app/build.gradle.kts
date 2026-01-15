11183f3b3b669e8e255d42fa0d0f29cc500c846c
heiligbasil <basilief@yahoo.com>
2026-01-15 17:55:30 -0600

    Upgrade SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
     namespace = "com.heiligbasil.datenight"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.heiligbasil.datenight"
         minSdk = 27
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
